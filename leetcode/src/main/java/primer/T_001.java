d60ca82d1367f5e6a2121ea1f777cf34cdf19700
hx1098 <1098269542@qq.com>
2020-12-14 09:03:55 +0800

    打印32位整型数据

--- leetcode/src/main/java/primer/T_001.java
+++ leetcode/src/main/java/primer/T_001.java
@@ -5,6 +5,17 @@ package primer;
  * @Title: <br>
  * @Description: <br>
  * @date 2020/12/13 23:16
+ * 打印32位数字的小程序.
  */
 public class T_001 {
+    /*int 类型的数据在java中的类型是32位的. */
+    public static void main(String[] args) {
+        int num = 783456348;
+        print(num);
+    }
+    public static void print(int num){
+        for (int i = 31; i >= 0; i--) {
+            System.out.println((num & (1 << i))==0 ? "0" : 1);
+        }
+    }
 }
