981de904fd214a723d238ddd07c58c8d55e7892b
hx1098 <1098269542@qq.com>
2021-07-13 13:26:45 +0800

    打印32位整型数据, 冒泡排序,

--- leetcode/src/main/java/primer/T_001.java
+++ leetcode/src/main/java/primer/T_001.java
@@ -1,6 +1,10 @@
 package primer;
 
+import jdk.internal.org.objectweb.asm.tree.MethodInsnNode;
 import org.junit.Test;
+
+import java.io.PrintStream;
+import java.util.jar.JarEntry;
 
 /**
  * @author hx   <br>
@@ -14,22 +18,31 @@ import org.junit.Test;
 public class T_001 {
 
 
-    /**
-     * 位移运算符,每一个
-     */
     @Test
     public void test1() {
         Integer test = 1;
         print(test);
+        /*向左移动1个位置*/
         print(test << 1);
+        /*向左移动八个位置*/
         print(test << 8);
+        System.out.println("================================");
+
+        int a = 2;
+        int b = 3;
+        print(a);
+        print(b);
+        int c = a & b;
+        print(c);
+        System.out.println(c);
+
     }
 
-    /** 
-     * @author hx
-     * @date 2021/6/1 15:38
+    /**
      * @param
      * @return
+     * @author hx
+     * @date 2021/6/1 15:38
      * @desc
      */
     @Test
@@ -48,7 +61,7 @@ public class T_001 {
         print(11);
 
         System.out.println("========================");
-        int[] arr = {9,3,5,2,4,6};
+        int[] arr = {9, 3, 5, 2, 4, 6};
         bubbo(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
@@ -59,6 +72,9 @@ public class T_001 {
 
     /**
      * 打印整形的32位二进制数据
+     * 位移运算符, 第一次 左移 31位,
+     * 第二次 左移 30位,
+     *
      * @param num
      */
     public static void print(int num) {
@@ -68,24 +84,51 @@ public class T_001 {
         System.out.println();
     }
 
+
     @Test
-    public void test18(){
+    public void test18() {
         print(-11);
     }
 
     /**
-     * 选择排序
+     * 打印数组
      *
      * @param arr
      */
-    public static void selectSort(int[] arr) {
-        int N = arr.length;
-        if (arr == null || N < 2) {
-            return;
-        }
-        for (int i = 0; i < N; i++) {
+    public static void printArr(int[] arr) {
+        for (int i : arr) {
 
+            System.out.print(i);
         }
+    }
+
+
+    /**
+     * 选择排序
+     * 用一个变量值记录最小值的下标,
+     * 思路: 第一遍: 找出最小的值   比较次数: arr.size 个数  最后交换位置
+     * 第二遍: 找出最小的值,  比较次数: arr.lenth-1    最后交换位置
+     */
+    @Test
+    public void selectSort() {
+        int arr[] = {5, 4, 2, 1, 3};
+        printArr(arr);
+
+        for (int j = 0; j < arr.length; j++) {
+            System.out.println();
+            int min = j;
+            for (int i = j + 1; i < arr.length; i++) {//每一次只找出最小的值
+                if (arr[min] > arr[i]) {
+                    min = i;
+                }
+            }
+            int tem = arr[j];
+            arr[j] = arr[min];
+            arr[min] = tem;
+            printArr(arr);
+        }
+
+
     }
 
     /**
@@ -119,12 +162,13 @@ public class T_001 {
 
     /**
      * 使用亦或进行两个数的交换
+     *
      * @param arr
      * @param a
      * @param b
      * @return
      */
-    public static int[] swap1(int[] arr,int a, int b){
+    public static int[] swap1(int[] arr, int a, int b) {
         arr[a] = arr[a] ^ arr[b];
         arr[b] = arr[a] ^ arr[b];
         arr[a] = arr[a] ^ arr[b];
@@ -148,7 +192,7 @@ public class T_001 {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    swap1(arr,j,j+1);
+                    swap1(arr, j, j + 1);
                 }
             }
         }
@@ -159,11 +203,11 @@ public class T_001 {
     }
 
     /*
-    * 两个数值互相交换， 不使用第三个变量的情况下
-    *
-    * 任何一个数 亦或上自己本身, 都是0, 任何数疑惑上0都是自己本身
-    *
-    */
+     * 两个数值互相交换， 不使用第三个变量的情况下
+     *
+     * 任何一个数 亦或上自己本身, 都是0, 任何数疑惑上0都是自己本身
+     *
+     */
     @Test
     public void testaBB() {
         int a = 1;
@@ -175,6 +219,22 @@ public class T_001 {
         System.out.println(b);
     }
 
+    /**
+     * 10的阶乘
+     */
+    public int test1(int num) {
+        int temp = 0;
+        int cusor = 1;
+        for (int i = 1; i <= num; i++) {
+            cusor = cusor * i;
+            temp = temp + cusor;
+        }
+        return temp;
+    }
 
+    @Test
+    public void test00() {
+        System.out.println(test1(3));
+    }
 
 }
