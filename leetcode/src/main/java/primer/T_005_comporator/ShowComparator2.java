e19a39b07be5fbdf05200dabd111d3f6bac7d89d
hx1098 <1098269542@qq.com>
2021-09-19 13:21:11 +0800

    1

---
+++ leetcode/src/main/java/primer/T_005_comporator/ShowComparator2.java
@@ -0,0 +1,36 @@
+package primer.T_005_comporator;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * @author hx
+ * @createTime 2021/9/3 9:35
+ * @version 1.0.0
+ * @description 自定义comparator
+ * @editUser hx
+ * @editTime 2021/9/3 9:35
+ * @editDescription
+ */
+public class ShowComparator2 {
+
+    public static class Student{
+        public String name;
+        public int age;
+        public int id;
+
+        public Student(String name, int age, int id) {
+            this.name = name;
+            this.age = age;
+            this.id = id;
+        }
+    }
+
+
+
+
+
+
+
+
+}
