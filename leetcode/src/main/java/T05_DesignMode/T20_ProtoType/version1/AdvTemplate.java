24f5de67136a230dfa291bdb253afd1c760e1b8b
hx1098 <1098269542@qq.com>
2021-10-30 08:47:12 +0800

    原型模式

---
+++ leetcode/src/main/java/T05_DesignMode/T20_ProtoType/version1/AdvTemplate.java
@@ -0,0 +1,29 @@
+package T05_DesignMode.T20_ProtoType.version1;
+
+/**
+ * @author hx
+ * @version 1.0.0
+ * @createTime 2021/10/30 8:24
+ * @description
+ * @editUser hx
+ * @editTime 2021/10/30 8:24
+ * @editDescription  广告模版接口
+ */
+public class AdvTemplate {
+
+    /**广告信名称*/
+    private String advSubject = "xx银行国庆信用卡抽奖活动";
+
+    /**广告信内容*/
+    private String advContext = "国庆抽奖活动通知: 只要刷卡就送你一百万!";
+
+    /**获取广告信的名称*/
+    public String getAdvSubject() {
+        return advSubject;
+    }
+
+    /**获取广告信的内容*/
+    public String getAdvContext() {
+        return advContext;
+    }
+}
---
+++ leetcode/src/main/java/T05_DesignMode/T20_ProtoType/version1/Client.java
@@ -0,0 +1,56 @@
+package T05_DesignMode.T20_ProtoType.version1;
+
+import java.util.Random;
+
+/**
+ * @author hx
+ * @version 1.0.0
+ * @createTime 2021/10/30 8:32
+ * @description
+ * @editUser hx
+ * @editTime 2021/10/30 8:32
+ * @editDescription 又到了如何使用的阶段了, 哈哈哈, 来看看这个是如何调用的
+ */
+public class Client {
+
+    /**
+     * 发送账单的数量, 这儿值是从数据库中获取的
+     */
+    private static int MAX_COUNT = 6;
+
+
+    public static void main(String[] args) {
+        //模拟发送邮件
+        int i = 0;
+        //    把模版定义出来, 这个是从数据库中获取
+        Mail mail = new Mail(new AdvTemplate());
+        mail.setTail("xx银行版权所有");
+        while (i < MAX_COUNT) {
+            //    一下是每封邮件不同的地方
+            mail.setAppellation(getRandString(5)+"先生(女士)");
+            mail.setReceiver(getRandString(5) + "@" + getRandString(8)+".com");
+            sendMail(mail);
+            i++;
+        }
+
+
+    }
+
+    public static void sendMail(Mail mail) {
+        System.out.println("标题:" + mail.getSubject() +"\t收件人:" + mail.getReceiver()+"\t...发送成功!" );
+    }
+
+    //获得指定长度的随机字符串
+    public static String getRandString(int maxLength) {
+        String source = "abcdefghijklmnopqrskuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuffer sb = new StringBuffer();
+        Random rand = new Random();
+        for (int i = 0; i < maxLength; i++) {
+            sb.append(source.charAt(rand.nextInt(source.length())));
+        }
+        return sb.toString();
+
+    }
+
+
+}
