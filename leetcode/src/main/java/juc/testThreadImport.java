b5f8b61068a70c012174c5ba627feb6f8814fbdf
hx1098 <1098269542@qq.com>
2022-01-18 23:13:16 +0800

    堆排序模型

---
+++ leetcode/src/main/java/juc/testThreadImport.java
@@ -0,0 +1,212 @@
+package juc;
+
+
+import cn.hutool.core.bean.BeanUtil;
+import org.apache.commons.beanutils.BeanUtils;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+/**
+ * @author hx
+ * @version 1.0.0
+ * @createTime 2022/1/13 14:24
+ * @description
+ * @editUser hx
+ * @editTime 2022/1/13 14:24
+ * @editDescription 这个future是多线程批量添加数据
+ */
+public class testThreadImport {
+
+    //@Transactional(rollbackFor = Exception.class)
+  /*  public int addFreshStudentsNew(List<FreshStudentAndStudentModel> list, String schoolNo) {
+        if (list == null || list.isEmpty()) {
+            return 0;
+        }
+        *//*List<StudentEntity> studentEntityList = new LinkedList<>();
+        List<EnrollStudentEntity> enrollStudentEntityList = new LinkedList<>();
+        List<AllusersEntity> allusersEntityList = new LinkedList<>();
+
+        list.forEach(freshStudentAndStudentModel -> {
+            EnrollStudentEntity enrollStudentEntity = new EnrollStudentEntity();
+            StudentEntity studentEntity = new StudentEntity();
+            BeanUtil.copyProperties(freshStudentAndStudentModel, studentEntity);
+            BeanUtil.copyProperties(freshStudentAndStudentModel, studentEntity);
+            BeanUtil.copyProperties(freshStudentAndStudentModel, enrollStudentEntity);
+            String operator = UUID.randomUUID().toString();
+            String studentId = UUID.randomUUID().toString();
+            enrollStudentEntity.setId(UUID.randomUUID().toString());
+            enrollStudentEntity.setStudentId(studentId);
+            enrollStudentEntity.(setIdentityCardIdfreshStudentAndStudentModel.getIdCard());
+            enrollStudentEntity.setOperator(operator);
+            studentEntity.setId(studentId);
+            studentEntity.setIdentityCardId(freshStudentAndStudentModel.getIdCard());
+            studentEntity.setOperator(operator);
+            studentEntityList.add(studentEntity);
+            enrollStudentEntityList.add(enrollStudentEntity);
+
+            AllusersEntity allusersEntity = new AllusersEntity();
+            allusersEntity.setId(enrollStudentEntity.getId());
+            allusersEntity.setUserCode(enrollStudentEntity.getNemtCode());
+            allusersEntity.setUserName(enrollStudentEntity.getName());
+            allusersEntity.setSchoolNo(schoolNo);
+            allusersEntity.setTelNum(enrollStudentEntity.getTelNum());
+            allusersEntity.setPassword(enrollStudentEntity.getNemtCode());  //密码设置为考生号
+            allusersEntityList.add(allusersEntity);
+        });*//*
+
+
+        int nThreads = 50;
+
+        int size = enrollStudentEntityList.size();
+        ExecutorService executorService = Executors.newFixedThreadPool(nThreads);
+        List<Future<Integer>> futures = new ArrayList<Future<Integer>>(nThreads);
+
+        //size = 160
+        //第一轮: start: 160/50*0 = 0 , end: 160/50*(0+1)=
+
+        for (int i = 0; i < nThreads; i++) {
+            final List<EnrollStudentEntity> EnrollStudentEntityImputList = enrollStudentEntityList.subList(size / nThreads * i, size / nThreads * (i + 1));
+            final List<StudentEntity> studentEntityImportList = studentEntityList.subList(size / nThreads * i, size / nThreads * (i + 1));
+            final List<AllusersEntity> allusersEntityImportList = allusersEntityList.subList(size / nThreads * i, size / nThreads * (i + 1));
+
+            Callable<Integer> task1 = () -> {
+                studentSave.saveStudent(EnrollStudentEntityImputList,studentEntityImportList,allusersEntityImportList);
+                return 1;
+            };
+            futures.add(executorService.submit(task1));
+        }
+        executorService.shutdown();
+        if (!futures.isEmpty() && futures != null) {
+            return 10;
+        }
+        return -10;
+    }
+*/
+    //测试100次
+    // 2 个:    6
+    // 4 个:    6
+    // 8 个：   7
+    // 16 个：  7
+    // 24 个：  7
+    // 50 个：  9
+    // 100 个：  14
+
+    public static void main(String[] args) {
+        List<Integer> times = new ArrayList<>();
+        for (int j = 0; j < 100; j++) {
+            long start = System.currentTimeMillis();
+            List<String> enrollStudentEntityList = new ArrayList<>();
+            for (int i = 0; i < 160; i++) {
+                enrollStudentEntityList.add(UUID.randomUUID().toString());
+            }
+
+            int nThreads = 1;
+            int size = 160;
+            ExecutorService executorService = Executors.newFixedThreadPool(nThreads);
+            List<Future<Integer>> futures = new ArrayList<Future<Integer>>(nThreads);
+
+            for (int i = 0; i < nThreads; i++) {
+                final List<String> integers = enrollStudentEntityList.subList(size / nThreads * i, size / nThreads * (i + 1));
+
+                int finalI = i;
+                Callable<Integer> task1 = () -> {
+                    System.out.println("第" + (finalI + 1) + "批保存的数据");
+                    return 1;
+                };
+                futures.add(executorService.submit(task1));
+            }
+            executorService.shutdown();
+            if (!futures.isEmpty() && futures != null) {
+                System.out.println("10");
+            } else {
+                System.out.println(-10);
+            }
+            Integer integer = Integer.valueOf((int) (System.currentTimeMillis() - start));
+            System.out.println( "耗费时间: " + integer);
+            times.add(integer);
+        }
+
+        int a = 0;
+        System.out.println("time.size" + times.size());
+        for (int i = 0; i < times.size(); i++) {
+            System.out.println(times.get(i));
+            a = a + times.get(i);
+        }
+        System.out.print("平均值： ");
+        System.out.println(a / times.size());
+    }
+
+
+    public static void main2(String[] args) throws ExecutionException, InterruptedException {
+        long star = System.currentTimeMillis();
+        CompletableFuture<String> startFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            for (int i = 0; i < 1000; i++) {
+                System.out.println("thread startFuture -11111111111");
+            }
+            return "12";
+        });
+        CompletableFuture<String> endFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            for (int i = 0; i < 1000; i++) {
+                System.out.println("thread startFuture -2222222222");
+            }
+            return "13";
+        });
+        CompletableFuture.allOf(startFuture, endFuture);
+        String startResult = startFuture.get();
+        String endResult = endFuture.get();
+        System.out.println(startResult + "  " + endResult);
+        System.out.println(System.currentTimeMillis() - star);
+
+
+    }
+
+
+    public static void main3(String[] args) throws ExecutionException, InterruptedException {
+        Object o = new Object();
+        long star = System.currentTimeMillis();
+        synchronized (o) {
+            CompletableFuture<String> startFuture = CompletableFuture.supplyAsync(() -> {
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                for (int i = 0; i < 1000; i++) {
+                    System.out.println("thread startFuture -11111111111");
+                }
+                return "12";
+            });
+            CompletableFuture<String> endFuture = CompletableFuture.supplyAsync(() -> {
+                try {
+                    TimeUnit.SECONDS.sleep(2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                for (int i = 0; i < 1000; i++) {
+                    System.out.println("thread startFuture -2222222222");
+                }
+                return "13";
+            });
+            CompletableFuture.allOf(startFuture, endFuture);
+            String startResult = startFuture.get();
+            String endResult = endFuture.get();
+            System.out.println(startResult + "  " + endResult);
+            System.out.println(System.currentTimeMillis() - star);
+        }
+
+
+    }
+
+
+}
---
+++ leetcode/src/main/java/leetcode/learning/T_07_HeapAndComprator/T_02_Heap.java
@@ -0,0 +1,112 @@
+package leetcode.learning.T_07_HeapAndComprator;
+
+import java.util.Arrays;
+
+/**
+ * @author hx
+ * @version 1.0.0
+ * @createTime 2022/1/17 13:35
+ * @description  堆的实现
+ * @editUser hx
+ * @editTime 2022/1/17 13:35
+ * @editDescription
+ *            1(0)
+ *       2(1)       3(2)
+ *   5(3)   6(4)  7(5)  8(6)
+ *
+ *  1.找到父节点推理:  (3-1)/2  = 1, 1位置就是父节点
+ *                   (4-1)/2  = 1, 1位置就是父节点
+ *   综上:
+ *     (i-1)/2 可找到自己的父亲
+ *
+ *  2.找到孩子节点的推理:
+ *                  2*0 +1= 1, 获取1位置的子节点
+ *                  2*0 +2= 2, 获取2位置的子节点
+ *                  2*1 +1= 3, 获取3位置的子节点
+ *     综上:
+ *        左孩子节点:  (i * 2) +1
+ *        右孩子节点:  (i * 2) +2
+ */
+
+public class T_02_Heap{
+
+    private static class MyMaxHeap{
+        private int[] heap;
+        private final int limit;
+        private int heapSize;
+
+        public MyMaxHeap(int limit) {
+            heap = new int[limit];
+            this.limit = limit;
+            heapSize = 0;
+        }
+
+        public boolean isEmpty() {
+            return heapSize == 0;
+        }
+        public boolean isFull() {
+            return heapSize == limit;
+        }
+        //这里需要heapInsert
+        public void push(int val) {
+            if (heapSize == limit)
+                throw new RuntimeException("heap is full");
+            heap[heapSize] = val;
+            //每次heapSize++ 之后, 该堆就会增加一个数据, 也即堆的孩子增加了一个.
+            heapInsert(heap, heapSize++);
+        }
+
+        //新加进来的数据, 需要跟上级进行比较,
+        //如果移动到了0位置, 或者干不掉自己的父亲的时候, 就需要停止下来.
+        private void heapInsert(int[] heap, int heapSize) {
+            //这里实际上是有两个条件的:
+            //1. index = 0 , heap[0]  heap[(0-1)/2]  自己本身和自己进行比 是相等的.
+            //2. index!= 0 , 正产执行的
+            while (heap[heapSize] > heap[(heapSize - 1) / 2]) {
+                swap(heap,heapSize,(heapSize-1)/2);
+                heapSize = (heapSize - 1) / 2;
+            }
+        }
+
+        //返回最大值, 并且在大根堆中, 把最大值删除掉
+        //剩下的数据, 仍然保持大根堆组织结构
+        public void pop() {
+            int ans = heap[0];
+            //将这个孩子 与 主干断开联系,
+            swap(heap, 0, --heapSize);
+            //看自己的孩子是否适合当
+            heapify(heap, 0, heapSize);
+        }
+
+        private void heapify(int[] heap, int index, int heapSize) {
+            //左孩子
+            int left = 2 * index + 1;
+            //看是否有左子树
+            while (left < heapSize) {
+                //看是否有右孩子,
+                //如果有右孩子, 并且有孩子的值比左海子的值大, 吧右孩子的下标赋值给largest
+                int largest = left + 1 < heapSize && heap[left + 1] > heap[left] ? left + 1 : left;
+                largest = heap[largest] > heap[index] ? largest : index;
+                if (largest == index)
+                    break;
+                swap(heap, largest, index);
+                index = largest;
+                left = index * 2 + 1;
+            }
+        }
+
+        private void swap(int[] arr, int i, int j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(-1 % 2);
+        System.out.println(1 / 2);
+        System.out.println(3 / 2);
+        System.out.println(4 / 2);
+    }
+
+}
