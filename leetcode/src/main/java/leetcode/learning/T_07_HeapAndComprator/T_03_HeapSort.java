3f307a7e3aececb92ebe2421c806881d6343c7f7
hx1098 <1098269542@qq.com>
2022-01-23 22:38:47 +0800

    堆排序

--- leetcode/src/main/java/leetcode/learning/T_07_HeapAndComprator/T_03_HeapSort.java
+++ leetcode/src/main/java/leetcode/learning/T_07_HeapAndComprator/T_03_HeapSort.java
@@ -16,7 +16,38 @@ public class T_03_HeapSort {
         if (arr == null || arr.length < 2) {
             return;
         }
+        for (int i = arr.length - 1; i >= 0; i--) {
+            heapify(arr, i, arr.length);
+        }
+        int heapSize = arr.length;
+        swap(arr, 0, --heapSize);
+        while (heapSize > 0) {
+            heapify(arr, 0, heapSize);
+            swap(arr, 0, --heapSize);
+        }
+
+
+
         
+    }
+    private static void heapify(int[] arr, int index, int heapSize) {
+        int left = index * 2 + 1;
+        while (left < heapSize) {
+            int largest = heapSize >= (left + 1)
+                    && arr[left] > arr[left + 1] ? left : left + 1;
+            largest = arr[largest] > arr[index] ? largest : index;
+            if (index == largest)
+                return;
+            swap(arr, index, largest);
+            index = largest;
+            left = left * 2 + 1;
+        }
+    }
+    //数据交换
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 
 }
