c2dfa3b80c3e5188e31270ec54101fdba183c003
hx1098 <1098269542@qq.com>
2022-01-27 15:05:52 +0800

    选择排序进行测试

--- leetcode/src/main/java/leetcode/learning/T_01_sort/T_01_SelectSort.java
+++ leetcode/src/main/java/leetcode/learning/T_01_sort/T_01_SelectSort.java
@@ -1,5 +1,10 @@
 package leetcode.learning.T_01_sort;
 
+import org.junit.Test;
+
+import java.io.CharArrayReader;
+import java.util.Arrays;
+
 /**
  * @author hx
  * @version 1.0.0
@@ -42,11 +47,106 @@ public class T_01_SelectSort {
     }
 
     public static void main(String[] args) {
-        int[] arr = {10, 8, 4, 9};
+        /*int[] arr = {10, 8, 4, 9};
         selectSort(arr);
         for (int i : arr) {
             System.out.print(i);
+        }*/
+
+
+        int testTime = 50000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generaRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            selectSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                printArray(arr1);
+                printArray(arr2);
+                break;
+            }
         }
+        System.out.println(succeed ? "Nice" : "effor");
+
+       /* int[] arr = generaRandomArray(maxSize, maxValue);
+        printArray(arr);
+        selectSort(arr);
+        printArray(arr);*/
+
     }
+
+    private static void printArray(int[] arr2) {
+        if(arr2 == null) return;
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.println(arr2[i]);
+        }
+        System.out.println();
+    }
+
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void comparator(int[] arr2) {
+        Arrays.sort(arr2);
+    }
+
+    private static int[] copyArray(int[] arr1) {
+        if(arr1 == null) return null;
+        int[] arrResult = new int[arr1.length];
+        for (int i = 0; i < arr1.length; i++) {
+            arrResult[i] = arr1[i];
+        }
+        return arrResult;
+    }
+
+    @Test
+    public void test1(){
+        for (int i = 0; i <100; i++) {
+            //System.out.println(Math.random());
+            double random = Math.random();
+            int temp = (int) ((100+1) * random);
+            if (temp == 100) {
+                System.out.println(random);
+            }
+            System.out.println(temp);
+        }
+
+    }
+
+
+
+    private static int[] generaRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            // [-? , +?], 使其有正数有负数
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+            //System.out.println(arr[i]);
+        }
+        return arr;
+    }
+    @Test
+    public void test12(){
+        generaRandomArray(1000, 1000);
+    }
+
 
 }
