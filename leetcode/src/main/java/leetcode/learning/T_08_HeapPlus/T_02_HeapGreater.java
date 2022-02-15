1c28e81cb8906dac437f7b5d60387c1b5702c78c
hx1098 <1098269542@qq.com>
2022-02-15 22:33:56 +0800

    加强堆

--- leetcode/src/main/java/leetcode/learning/T_08_HeapPlus/T_02_HeapGreater.java
+++ leetcode/src/main/java/leetcode/learning/T_08_HeapPlus/T_02_HeapGreater.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
  * @description
  * @editUser hx
  * @editTime 2022/2/10 9:25
- * @editDescription  加强堆的实现
+ * @editDescription  加强堆的实现(利用了反向索引)
  */
 public class T_02_HeapGreater<T> {
 
@@ -46,8 +46,61 @@ public class T_02_HeapGreater<T> {
         heapInsert(heapSize++);
     }
 
-    private void heapInsert(int i) {
-        
+    public T pop() {
+        T ans = heap.get(0);
+        //和末尾位置作交换
+        swap(0, heapSize - 1);
+        indexMap.remove(ans);
+        heap.remove(--heapSize);
+        heapify(0);
+        return ans;
     }
+
+    public void remove(T obj) {
+        T replace = heap.get(heapSize - 1);
+        int index = indexMap.get(obj);
+        indexMap.remove(obj);
+        heap.remove(--heapSize);
+        if (replace != obj) {
+
+        }
+    }
+
+    private void heapify(int index) {
+        int left = 2 * index + 1;
+        while (left < heapSize) {
+            //有右节点, 且右节点大
+            int best = left + 1 < heapSize && comp.compare(heap.get(left), heap.get(left + 1)) < 0 ? (left + 1) : left;
+            //todo 看下这个compare == 0 , ==-1 时候到底是谁大谁小
+            best = comp.compare(heap.get(best), heap.get(index)) < 0 ? best : index;
+            if (best == index) {
+                break;
+            }
+            swap(best,index);
+            index = best;
+            left = 2 * index + 1;
+        }
+    }
+
+
+    private void heapInsert(int index) {
+        //(3-1)/2 是求出父节点
+        while (comp.compare(heap.get(index), heap.get((index - 1) / 2)) < 0) {
+            swap(index, (index - 1) / 2);
+            index = (index - 1) / 2;
+        }
+    }
+
+    //互换的时候将值换掉, 同时将反向索引也要换掉
+    private void swap(int i, int j) {
+        T o1 = heap.get(i);
+        T o2 = heap.get(i);
+        heap.set(i, o2);
+        heap.set(j, o1);
+        indexMap.put(o2, i);
+        indexMap.put(o1, j);
+    }
+
+
 
 }
