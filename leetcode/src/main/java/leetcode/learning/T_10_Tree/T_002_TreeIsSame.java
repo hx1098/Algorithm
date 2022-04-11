61ba2c8581bb27c78e3ce0f3f2384a4a66aeda4d
hx1098 <1098269542@qq.com>
2022-04-11 09:01:44 +0800

    查询一个树是否是镜面树

--- leetcode/src/main/java/leetcode/learning/T_10_Tree/T_002_TreeIsSame.java
+++ leetcode/src/main/java/leetcode/learning/T_10_Tree/T_002_TreeIsSame.java
@@ -13,23 +13,24 @@ public class T_002_TreeIsSame {
 
     public class TreeNode{
         private int val;
-        private TreeNode right;
         private TreeNode left;
+        private TreeNode right;
 
         public TreeNode(int val) {
             this.val = val;
         }
-
     }
 
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null ^ q == null) {
-            return false;
-        }
-        if (p == null && q == null) {
-            return true;
-        }
-        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    public static void main(String[] args) {
+        String a = null;
+        String b = null;
+        System.out.println(a == null ^ b == null);
     }
 
+    public static boolean isSymmtricTreee(TreeNode p1, TreeNode p2) {
+        //只要是两个不同的就返回false
+        if (p1 == null ^ p2 == null) return false;
+        if (p1 == null && p2 == null) return true;
+        return p1.val == p2.val && isSymmtricTreee(p1.left, p2.right) && isSymmtricTreee(p1.right, p2.left);
+    }
 }
