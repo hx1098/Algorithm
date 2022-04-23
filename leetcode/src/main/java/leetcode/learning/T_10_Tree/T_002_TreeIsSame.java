1977c74991cda1e8f5a8f7a13d20c0ab9029c3c3
hx1098 <1098269542@qq.com>
2022-04-23 22:35:24 +0800

    判断问题理解

--- leetcode/src/main/java/leetcode/learning/T_10_Tree/T_002_TreeIsSame.java
+++ leetcode/src/main/java/leetcode/learning/T_10_Tree/T_002_TreeIsSame.java
@@ -24,12 +24,15 @@ public class T_002_TreeIsSame {
     public static void main(String[] args) {
         String a = null;
         String b = null;
+        System.out.println(a == b);
+        System.out.println(a == null ^ b == null);
+        a = "11";
         System.out.println(a == null ^ b == null);
     }
 
     public static boolean isSymmtricTreee(TreeNode p1, TreeNode p2) {
         //只要是两个不同的就返回false
-        if (p1 == null ^ p2 == null) return false;
+        if (p1 == null ^ p2 == null) return false;//一个为空,一个不为空时候是条件成立, 两个都为空, 或者两个都不为空时候, 条件不成立
         if (p1 == null && p2 == null) return true;
         return p1.val == p2.val && isSymmtricTreee(p1.left, p2.right) && isSymmtricTreee(p1.right, p2.left);
     }
