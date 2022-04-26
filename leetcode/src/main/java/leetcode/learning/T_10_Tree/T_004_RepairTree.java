ea2ceefd19ea0627b69b275d4de2188ed7427e25
hx1098 <1098269542@qq.com>
2022-04-27 07:33:07 +0800

    使用hash表重组二叉树

--- leetcode/src/main/java/leetcode/learning/T_10_Tree/T_004_RepairTree.java
+++ leetcode/src/main/java/leetcode/learning/T_10_Tree/T_004_RepairTree.java
@@ -1,5 +1,7 @@
 package leetcode.learning.T_10_Tree;
 
+import java.util.HashMap;
+
 /**
  * @author hx
  * @version 1.0.0
@@ -44,6 +46,38 @@ public class T_004_RepairTree {
         }
         head.left = f(pre, L1, L1 + find - L2, in, L2, find-1);
         head.right = f(pre, L1 + find - L2 + 1, R1, in, find + 1, R2);
+        return head;
+    }
+
+
+    /**
+     * 这种方法是使用一个hash表来存储, 头节点在 中序遍历中的位置, 来确定左树和右树 在数组中的位置.
+     * @param pre
+     * @param in
+     * @return
+     */
+    public static TreeNode buidlTre2(int[] pre, int[] in) {
+        if (pre == null || in == null || pre.length != in.length) {
+            return null;
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < in.length; i++) {
+            map.put(in[i], i);
+        }
+        return f2(pre, 0, pre.length - 1, in, 0, in.length - 1,map);
+    }
+    public static TreeNode f2(int[] pre, int L1, int R1, int[] in, int L2, int R2,HashMap<Integer,Integer> map) {
+        //先 : 123
+        //中 : 231  这是一个不规则的树形结构, 防止左树为空, 或者右树为空的情况
+        if (L1 > R1) return null;
+        TreeNode head = new TreeNode(pre[L1]);
+        if (L1 == R1) {
+            return head; //此时说明只有一个节点, 不需要向下找了
+        }
+        //在中序遍历中找到头结点
+        int find = map.get(pre[L1]);
+        head.left = f2(pre, L1, L1 + find - L2, in, L2, find-1,map);
+        head.right = f2(pre, L1 + find - L2 + 1, R1, in, find + 1, R2,map);
         return head;
     }
 
